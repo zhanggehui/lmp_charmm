75f87d3d9369ecba9033b03ff80fab9461f30de7
zhanggehui <395510548@qq.com>
2020-11-19 17:38:37 +0800

    update

--- Scripts/sub_lmp.sh
+++ Scripts/sub_lmp.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH -J lmp
-#SBATCH -p cn-short
+#SBATCH -p cn-long
 #SBATCH -N 4
-#SBATCH --qos=liufengcns
+#SBATCH --qos=liufengcnl
 #SBATCH --ntasks-per-node=20
 #SBATCH -o 1.out
 #SBATCH -e 2.err
@@ -11,12 +11,7 @@
 #SBATCH --exclusive
 
 hosts=`scontrol show hostname $SLURM_JOB_NODELIST`; echo $hosts
-echo aaaa
-echo $SLURM_JOB_NODELIST
-echo aaaa
-partion=`scontrol show hostname`
-echo ${partion}
-echo aaaa
+
 source /home/liufeng_pkuhpc/lustre2/zgh/zgh_lmp/lmp_install/lammps_29Oct2020.sh $SLURM_JOB_PARTITION
 
 mpirun -np $SLURM_NTASKS lmp -in step4.2_annealing.inp
