d3ec4981c5af78b927fcdaa1b04181b4bab59b53
zhanggehui <395510548@qq.com>
2020-11-19 17:21:32 +0800

    update

--- Scripts/sub_lmp.sh
+++ Scripts/sub_lmp.sh
@@ -11,11 +11,9 @@
 #SBATCH --exclusive
 
 hosts=`scontrol show hostname $SLURM_JOB_NODELIST`; echo $hosts
-partion=`scontrol show partition`
+partion=`scontrol show partition $SLURM_JOB_PARTITION`
 echo ${partion}
 
-currentdir=`pwd` 
-cd /home/liufeng_pkuhpc/lustre2/zgh/zgh_lmp/lmp_install/ ; gitget ; cd $currentdir
 source /home/liufeng_pkuhpc/lustre2/zgh/zgh_lmp/lmp_install/lammps_29Oct2020.sh ${partion}
 
 mpirun -np $SLURM_NTASKS lmp -in step4.2_annealing.inp
